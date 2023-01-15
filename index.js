dbc6e0319849ad61ad8657c5417d2c2b23c9d116
Justin Brown <Justinben2335@gmail.com>
2023-01-15 16:25:49 -0600

    Added quant of photos to js file.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const submitButton = document.getElementById('submit');
 const dimInput = document.getElementById('dim');
 const url = 'https://api.nasa.gov/planetary/earth/assets';
 const imgDiv = document.getElementById('images');
+const numPhotos = document.getElementById('numberOfPhotos');
 
 
 
@@ -14,10 +15,11 @@ async function submit() {
     let lat = latInput.value;
     let date = dateInput.value;
     let dim =dimInput.value;
+    let quant = numPhotos.value;
     let urlArr = [];
     let imgArr = [];
     let count = 0;
-    for (i=0; i <= 1; i++){
+    for (i=0; i <= quant; i++){
         let urlToFetch = `${url}?lon=${long + (i*100)}&lat=${lat + (i*100)}&dim=${dim}&date=${date}&api_key=${apiKey}`;
         let response = await fetch(urlToFetch)
         let data = await response.json();
@@ -26,6 +28,8 @@ async function submit() {
     }
     for (img of imgArr){
     img.setAttribute('src', urlArr[count]);
+    img.setAttribute('width', `${100/quant}%`);
+    img.setAttribute('height', `${100/quant}%`);
     img.setAttribute('class', 'satImage');
     imgDiv.appendChild(img)
     }
