d61ce1405d76721fda12455808058964546b79f2
Justin Brown <Justinben2335@gmail.com>
2023-01-19 14:42:19 -0700

    Got row and column to work

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const submitButton = document.getElementById('submit');
 const dimInput = document.getElementById('dim');
 const url = 'https://api.nasa.gov/planetary/earth/assets';
 const imgDiv = document.getElementById('images');
-const numPhotos = document.getElementById('numberOfPhotos');
+const rowsOfPhotos = document.getElementById('rowsOfPhotos');
+const colsOfPhotos = document.getElementById('columnsOfPhotos');
 const loading = document.getElementById('loading')
 
 
@@ -16,26 +17,51 @@ async function submit() {
     let lat = Number(latInput.value).toFixed(2);
     let date = dateInput.value;
     let dim =Number(dimInput.value);
-    let quant = numPhotos.value;
+    let rowQuant = rowsOfPhotos.value;
+    let colQuant = colsOfPhotos.value;
     let urlArr = [];
     let imgArr = [];
     let count = 0;
+    let longToFetch;
+    let latToFetch;
+    let testArr = [];
     //console.log(`Long: ${long} Lat: ${lat}`)
     // console.log(`returnIncreasedDec(.15, 3): ${returnIncreasedDec(.15, 3)}`)
-    for (let i=0; i <= quant - 1; i++){
-        console.log(`img: ${i + 1} | long: ${Number(long)} | lat: ${Number(lat) + (i * Number(dim))} | dim: ${dim}`)
-        let urlToFetch = `${url}?lon=${Number(Number(long) - (i * Number(dim / 2))).toFixed(2)}&lat=${Number(lat)}&dim=${dim}&date=${date}&api_key=${apiKey}`;
-        let response = await fetch(urlToFetch)
-        let data = await response.json();
-        urlArr.push(data.url);
-        imgArr.push(document.createElement('img'))
+
+        //FETCH ROWS
+    // for (let i=0; i <= rowQuant - 1; i++){
+    //     //console.log(`img: ${i + 1} | long: ${Number(long)} | lat: ${Number(lat) + (i * Number(dim))} | dim: ${dim}`)
+        
+    //     let urlToFetch = `${url}?lon=${Number(Number(long) - (i * Number(dim))).toFixed(2)}&lat=${Number(lat)}&dim=${dim}&date=${date}&api_key=${apiKey}`;
+    //     let response = await fetch(urlToFetch)
+    //     let data = await response.json();
+    //     urlArr.push(data.url);
+    //     imgArr.push(document.createElement('img'))
+    //     testArr=[];
+    // }
+
+        //FETCH COLUMNS
+    for (let i=0; i <= colQuant - 1; i++){
+        for (let j=0; j <= rowQuant - 1; j++){
+            latToFetch = Number(Number(lat) + (i * dim)).toFixed(2);
+            longToFetch = Number(Number(long) - (j * Number(dim))).toFixed(2);
+            //console.log(`i: ${i} | j: ${j} | Lat: ${latToFetch} | Long: ${longToFetch}`)
+            
+            let urlToFetch = `${url}?lon=${longToFetch}&lat=${latToFetch}&dim=${dim}&date=${date}&api_key=${apiKey}`;
+            let response = await fetch(urlToFetch)
+            let data = await response.json();
+
+            urlArr.push(data.url);
+            imgArr.push(document.createElement('img'))
+            testArr.push(`rowQuant i: ${i} | colQuant j: ${j} | lat: ${latToFetch} | lon: ${longToFetch}`);
+        }
     }
     
     for (img of imgArr){
         img.onload = function(){imgLoaded(img)};
         img.setAttribute('src', urlArr[count]);
-        img.setAttribute('width', `${100/quant}%`);
-        img.setAttribute('height', `${100/quant}%`);
+        img.setAttribute('width', `${100/rowQuant}%`);
+        img.setAttribute('height', `${100/rowQuant}%`);
         img.setAttribute('class', 'satImage');
         imgDiv.prepend(img)
         count += 1;
@@ -43,7 +69,7 @@ async function submit() {
     //console.log(urlArr);
     //console.log(imgArr);
     
-    
+    console.log(testArr);
 }
 
 function returnIncreasedDec(num, mult){
